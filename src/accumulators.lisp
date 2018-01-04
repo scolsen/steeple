42185c886ec458439cdbf609052ca11864aaf210
Scott Olsen <scg.olsen@gmail.com>
2018-01-04 12:30:45 -0500

    Refactor: move accumulators

---
+++ src/accumulators.lisp
@@ -0,0 +1,24 @@
+(defpackage steeple.accumulators
+    (:use :cl)
+    (:import-from :steeple.predicates
+                  :non-empty-list?)
+    (:export :accumulator
+             :accumulate-n))
+
+(in-package steeple.accumulators)
+
+(defun accumulator (name binary-fn initial-value) 
+    (setf (symbol-function name) 
+          (let ((x initial-value)) 
+               (lambda (&rest y) 
+                       (if (steeple.predicates:non-empty-list? y) 
+                           (setf x (apply binary-fn (append (list x) y))) 
+                           x)))))
+
+(defun accumulate-n (accum n &rest arguments) 
+    (let ((fn)) (if (steeple.predicates:non-empty-list? arguments) 
+          (setf fn (apply #'partial-l accum arguments)) 
+          (setf fn accum))
+      (dotimes (num n) 
+          (funcall fn))) (funcall accum))
+
